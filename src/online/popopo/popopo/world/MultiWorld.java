3b3a8a8a08b54c82fea10a53efe6878293c1fdb3
koshianpan <kurokogeanpan@gmail.com>
2017-08-31 01:45:43 +0900

    [#7] developed world generation/configuration function

---
+++ src/online/popopo/popopo/world/MultiWorld.java
@@ -0,0 +1,99 @@
+package online.popopo.popopo.world;
+
+import online.popopo.popopo.common.config.Configurable;
+import online.popopo.popopo.common.config.Parameter;
+import org.bukkit.World.Environment;
+import org.bukkit.WorldType;
+import org.bukkit.configuration.MemorySection;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class MultiWorld implements Configurable {
+    @Parameter("")
+    private MemorySection config = null;
+
+    public Set<String> getNames() {
+        if (this.config == null) {
+            return new HashSet<>();
+        } else {
+            return this.config.getKeys(false);
+        }
+    }
+
+    public WorldConfig getConfig(String name) {
+        if (this.config == null) {
+            return null;
+        } else if (!this.config.contains(name)) {
+            return null;
+        } else {
+            Map map = this.config.getValues(false);
+            Object o = map.get(name);
+
+            return new WorldConfig((MemorySection) o);
+        }
+    }
+
+    public static class WorldConfig {
+        private final MemorySection c;
+
+        private WorldConfig(MemorySection c) {
+            this.c = c;
+        }
+
+        public boolean hasEnvironment() {
+            return this.c.contains("environment");
+        }
+
+        public Environment getEnvironment() {
+            String n = this.c.getString("environment")
+                    .toUpperCase();
+
+            try {
+                return Environment.valueOf(n);
+            } catch (IllegalArgumentException e) {
+                return null;
+            }
+        }
+
+        public boolean hasSeed() {
+            return this.c.contains("seed");
+        }
+
+        public long getSeed() {
+            return this.c.getLong("seed");
+        }
+
+        public boolean hasWorldPreset() {
+            return this.c.contains("world_preset");
+        }
+
+        public String getWorldPreset() {
+            return this.c.getString("world_preset");
+        }
+
+        public boolean hasStructures() {
+            return this.c.contains("structures");
+        }
+
+        public boolean getStructures() {
+            return this.c.getBoolean("structures");
+        }
+
+        public boolean hasWorldType() {
+            return this.c.contains("world_type");
+        }
+
+        public WorldType getWorldType() {
+            String n = this.c.getString("world_type");
+
+            return WorldType.getByName(n);
+        }
+    }
+
+    @Override
+    public String getSectionName() {
+        return "worlds";
+    }
+}
---
+++ src/online/popopo/popopo/world/WorldLoader.java
@@ -0,0 +1,69 @@
+package online.popopo.popopo.world;
+
+import online.popopo.popopo.world.MultiWorld.WorldConfig;
+import org.bukkit.World.Environment;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+
+public class WorldLoader {
+    private final MultiWorld set;
+
+    public WorldLoader(MultiWorld w) {
+        this.set = w;
+    }
+
+    private boolean loadWorld(String name) {
+        WorldCreator c = WorldCreator.name(name);
+        WorldConfig w = this.set.getConfig(name);
+
+        if (w.hasEnvironment()) {
+            Environment e = w.getEnvironment();
+
+            if (e != null) {
+                c.environment(e);
+            } else {
+                return false;
+            }
+        }
+
+        if (w.hasSeed()) {
+            long seed = w.getSeed();
+
+            c.seed(seed);
+        }
+
+        if (w.hasStructures()) {
+            boolean structures = w.getStructures();
+
+            c.generateStructures(structures);
+        }
+
+        if (w.hasWorldPreset()) {
+            String worldPreset = w.getWorldPreset();
+
+            c.generatorSettings(worldPreset);
+        }
+
+        if (w.hasWorldType()) {
+            WorldType t = w.getWorldType();
+
+            if (t != null) {
+                c.type(t);
+            } else {
+                return false;
+            }
+        }
+
+        return c.createWorld() != null;
+    }
+
+    public boolean loadWorlds() {
+        for (String n : this.set.getNames()) {
+            if (!loadWorld(n)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
