c51dfc3c9f730ab80da78d6629d8695bea6dbba0
koshianpan <kurokogeanpan@gmail.com>
2017-09-18 17:04:03 +0900

    prepared 1.0.0 beta

---
+++ README.md
@@ -0,0 +1,8 @@
+# Popopo
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+[![Build Status](https://travis-ci.org/koshianpan/Popopo.svg?branch=master)](https://travis-ci.org/koshianpan/Popopo)
+[![GitHub issues](https://img.shields.io/github/issues/koshianpan/Popopo.svg)](https://github.com/koshianpan/Popopo/issues)
+[![GitHub release](https://img.shields.io/github/release/koshianpan/Popopo.svg)](https://github.com/koshianpan/Popopo/releases)
+
+## Description
+Popopo is spigot plugin for minecraft server. This plugin provide many simply functions.
--- src/online/popopo/popopo/Main.java
+++ src/online/popopo/popopo/Main.java
@@ -1,9 +1,74 @@
 package online.popopo.popopo;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import online.popopo.common.PluginBase;
+import online.popopo.common.config.Property;
+import online.popopo.popopo.input.InputListener;
+import online.popopo.popopo.console.ConsoleCommand;
+import online.popopo.popopo.portal.Portal;
+import online.popopo.popopo.portal.PortalCommand;
+import online.popopo.popopo.portal.PortalListener;
+import online.popopo.popopo.protection.Judge;
+import online.popopo.popopo.protection.License;
+import online.popopo.popopo.protection.ProtectCommand;
+import online.popopo.popopo.protection.Reserve;
+import online.popopo.popopo.protection.listener.*;
+import online.popopo.popopo.selection.SelectCommand;
+import online.popopo.popopo.system.SystemCommand;
+import online.popopo.popopo.input.Japanese;
+import online.popopo.popopo.domain.TeleportListener;
+import online.popopo.popopo.world.TransferCommand;
+import online.popopo.popopo.world.WorldInfo;
+import online.popopo.popopo.world.WorldLoader;
 
-public class Main extends JavaPlugin {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main extends PluginBase {
+    @Property(key = "portals")
+    private Map<String, Portal> portals = new HashMap<>();
+    @Property(key = "reserves")
+    private Map<String, Reserve> reserves = new HashMap<>();
+
+    private Japanese jpn = new Japanese();
+    private Map<String, WorldInfo> worlds;
+    private Map<String, License> licenses;
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        loadTheme("theme.yml");
+        getIO().read(".data/data.gz", this);
+        getIO().readResource("kana.gz", jpn);
+        worlds = getIO().readDir("worlds", WorldInfo.class);
+        licenses = getIO().readDir("licenses", License.class);
+    }
+
     @Override
     public void onEnable() {
+        super.onEnable();
+
+        new WorldLoader().load(this, worlds);
+
+        Judge judge = new Judge(reserves, licenses);
+
+        register(new InputListener(this, jpn));
+        register(new TeleportListener(this));
+        register(new PortalListener(portals));
+        register(new BlockListener(judge));
+        register(new EntityListener(judge));
+        register(new ExplosionListener(judge));
+        register(new PlayerListener(judge));
+        register("select", new SelectCommand(this));
+        register("system", new SystemCommand());
+        register("console", new ConsoleCommand(this));
+        register("transfer", new TransferCommand());
+        register("portal", new PortalCommand(this, portals));
+        register("protect", new ProtectCommand(this, judge));
+    }
+
+    @Override
+    public void onDisable() {
+        getIO().write(".data/data.gz", this);
     }
 }
